63938f133d78e5bdee9e7901a933f1ecec9a385d
Daniele Testa <daniele.testa@gmail.com>
2022-01-11 17:33:30 +0000

    simple

--- main.tf
+++ main.tf
@@ -1,25 +1,6 @@
-terraform {
-  required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "~> 2.13.0"
-    }
+resource "test" "web" {
+
+  provisioner "local-exec" {
+    command = "env >> /tmp/env.txt"
   }
 }
-
-provider "docker" {}
-
-resource "docker_image" "nginx" {
-  name         = "nginx:latest"
-  keep_locally = false
-}
-
-resource "docker_container" "nginx" {
-  image = docker_image.nginx.latest
-  name  = "tutorial"
-  ports {
-    internal = 80
-    external = 8000
-  }
-}
-
